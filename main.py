39ae23284b0596d3c02e1ac502d6ca02ade789ef
severo98 <severoviviane98@gmail.com>
2024-12-02 15:49:26 -0400

    alteraçãodenomeprofissão

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-"""
-@author: Viviane
-"""
-from flask import Flask  #importa a biblioteca. flask diferente de Flask
+from flask import Flask, render_template
 
-app_Viviane = Flask (__name__) #cria objeto da aplicação. 
-                               # Tem 2 underlines antes e depois de name
+app_Viviane = Flask(__name__, template_folder='t_templates') 
+# os templates coloca em outra pasta. 
+# Por padrão, fica na pasta templates e não precisa informar no template_folder,
+# mas se quiser armazenar em outra pasta indique nesse parâmetro.
 
-@app_Viviane.route('/')
-@app_Viviane.route('/ola') # rota para solicitação web
-                           # @ significa decorador
-def raiz():                # função a ser executa quando chamar rota acima ('/ola')
-    return 'Olá, turma!'
+@app_Viviane.route("/")       #se no navegador digitar / ou /index
+@app_Viviane.route("/index")  
+def indice():
+    return render_template ("t_index.html") #optei por prefixar com t_ os nomes dos arquivos que usam template
 
-def saudacoes (nome):
-    return f'Olá, {nome}!'
+@app_Viviane.route("/contato")
+def contato():
+    return render_template("t_contato.html") 
 
-if __name__ == "__main__" : # Dois underlines no name e no main
-    # diferenciar quando é executado aqui, como um script princial
-    # ou é invocado por fora, quando importado por um módulo
-    app_Viviane.run(port = 8000)  #executa aplicação
+@app_Viviane.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""}) 
+def usuarios (nome_usuario, nome_profissao):
+    dados_usu = {"nome": "viviane", "profissao":"desenvolvedora"}
+    return render_template ("t_usuario.html", nome=nome_usuario, dados = dados_usu)  
+
+
+#rota /usuarios COM passagem de argumentos
+#@app_Viviane.route("/usuario/<nome_usuario>;<nome_profissao>")
+#rota /usuarios SEM passagem de argumentos --> definir valor padrão com defaults
+
+if __name__ == "__main__": 
+     app_Viviane.run(port = 8000) 
